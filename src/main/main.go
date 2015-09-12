a9e7119917bb4cedcb82bdd8dcb5162b0448b2af
obadonke <git@obadonke.com>
2015-09-12 13:43:31 +0100

    Serve a basic template

--- src/main/main.go
+++ src/main/main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"net/http"
+	"text/template"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		tmpl, err := template.New("test").Parse(doc)
+		if err != nil {
+			return
+		}
+
+		tmpl.Execute(w, nil)
+	})
+
+	http.ListenAndServe(":8000", nil)
 }
+
+const doc = `
+<!DOCTYPE html>
+<html>
+	<head><title>Example Title</title></head>
+	<body>
+		<h1>Hello Templates</h1>
+	</body>
+</html>
+`
