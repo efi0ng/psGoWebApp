a92b1431abfbcc0c15436f328c9ca3b69fd5198b
obadonke <git@obadonke.com>
2015-09-12 11:57:24 +0100

    Second iteration plus course files minus video that stuffed up my repo.

--- src/main/main.go
+++ src/main/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 )
 
-
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-			w.Write([]byte("Hello World"))
-		})
-	
+	http.Handle("/", new(MyHandler))
+
 	http.ListenAndServe(":8000", nil)
+}
+
+type MyHandler struct {
+	http.Handler
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	path := "public/" + req.URL.Path
+	data, err := ioutil.ReadFile(string(path))
+
+	if err != nil {
+		w.WriteHeader(404)
+		w.Write([]byte("404 - " + http.StatusText(404)))
+		return
+	}
+
+	w.Write(data)
 }
