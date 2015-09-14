19256b1c219c9c97f6ebafaa993a1668d77878f6
obadonke <git@obadonke.com>
2015-09-14 21:03:37 +0100

    added categories and category view models

--- src/main/main.go
+++ src/main/main.go
@@ -1,41 +1,45 @@
 package main
 
 import (
+	"bufio"
 	"net/http"
 	"os"
-	"text/template"
-	"bufio"
 	"strings"
+	"text/template"
 	"viewmodels"
 )
 
 func main() {
 	templates := populateTemplates()
-	
+
 	// handle templates
 	http.HandleFunc("/",
 		func(w http.ResponseWriter, req *http.Request) {
 			requestedFile := req.URL.Path[1:]
 			template := templates.Lookup(requestedFile + ".html")
-			
+
 			var context interface{} = nil
-			if requestedFile == "home" {
+
+			switch requestedFile {
+			case "home":
 				context = viewmodels.GetHome()
+			case "categories":
+				context = viewmodels.GetCategories()
 			}
-			
+
 			if template != nil {
 				template.Execute(w, context)
 			} else {
 				w.WriteHeader(404)
 			}
 		})
-	
+
 	// handle resources
 	http.HandleFunc("/img/", serveResource)
 	http.HandleFunc("/css/", serveResource)
 	http.HandleFunc("/video/", serveResource)
 
-	http.ListenAndServe(":8000", nil);
+	http.ListenAndServe(":8000", nil)
 }
 
 func serveResource(w http.ResponseWriter, req *http.Request) {
@@ -55,35 +59,35 @@ func serveResource(w http.ResponseWriter, req *http.Request) {
 	}
 
 	f, err := os.Open(path)
-	if (err != nil) {
+	if err != nil {
 		w.WriteHeader(404)
 		return
-	}	
-	
+	}
+
 	defer f.Close()
 	w.Header().Add("Content-Type", contentType)
-	
+
 	br := bufio.NewReader(f)
 	br.WriteTo(w)
 }
 
 func populateTemplates() *template.Template {
 	result := template.New("templates")
-	
+
 	basePath := "templates"
 	templateFolder, _ := os.Open(basePath)
 	defer templateFolder.Close()
-	
+
 	templatePathsRaw, _ := templateFolder.Readdir(-1)
-	
+
 	templatePaths := new([]string)
 	for _, pathInfo := range templatePathsRaw {
 		if !pathInfo.IsDir() {
-			*templatePaths = append(*templatePaths, basePath + "/" + pathInfo.Name())
+			*templatePaths = append(*templatePaths, basePath+"/"+pathInfo.Name())
 		}
 	}
-	
+
 	result.ParseFiles(*templatePaths...)
-	
+
 	return result
 }
