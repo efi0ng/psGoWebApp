b9872d4f7002ea668eae22451976f5bd3b984963
obadonke <git@obadonke.com>
2015-09-14 19:40:25 +0100

    Introduce the real template cache. Css etc not handled.

--- src/main/main.go
+++ src/main/main.go
@@ -1,81 +1,46 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 	"text/template"
-	"time"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Add("Content-Type", "text/html")
-		container := template.New("container")
-
-		_, err := container.New("main").Parse(doc)
-		if err != nil {
-			return
-		}
-
-		_, err = container.New("header").Parse(header)
-		if err != nil {
-			return
-		}
-
-		_, err = container.New("footer").Parse(footer)
-		if err != nil {
-			return
-		}
-
-		context := Context{
-			"Fruit Store Galore",
-			"Jake Jones",
-			req.URL.Path,
-			[4]string{"Lemon", "Orange", "Apple", "Pear"},
-		}
-		container.Lookup("main").Execute(w, context)
-	})
-
-	http.ListenAndServe(":8000", nil)
+	templates := populateTemplates()
+	
+	http.HandleFunc("/",
+		func(w http.ResponseWriter, req *http.Request) {
+			requestedFile := req.URL.Path[1:]
+			template := templates.Lookup(requestedFile + ".html")
+			
+			if template != nil {
+				template.Execute(w, nil)
+			} else {
+				w.WriteHeader(404)
+			}
+		})
+	
+	http.ListenAndServe(":8000", nil);
 }
 
-const doc = `
-{{template "header" .Title}}
-	<body>
-		{{if eq .Path "/Google"}}
-			<h1>Hey, Google made Go!</h1>
-		{{else}} 
-			<h1>Hola {{.Message}}!</h1>
-		{{end}}
-		<p>You have reached {{.Path}}. {{.Knowledge "hope"}}</p>
-		<ul>
-		{{range .Fruit}}
-		  <li>{{.}}</li>
-		{{else}}
-			<li>No fruit found?</li>
-		{{end}}
-		</ul>
-	</body>
-{{template "footer"}}
-`
-
-const header = `
-<!DOCTYPE html>
-<html>
-	<head><title>{{.}}</title></head>
-`
-
-const footer = `
-</html>
-`
-
-type Context struct {
-	Title   string
-	Message string
-	Path    string
-	Fruit   [4]string
-}
-
-func (this Context) Knowledge(tidbit string) string {
-	return fmt.Sprint("Wise wisdom (", tidbit, ") at ", time.Now().Format(time.UnixDate))
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	
+	basePath := "templates"
+	templateFolder, _ := os.Open(basePath)
+	defer templateFolder.Close()
+	
+	templatePathsRaw, _ := templateFolder.Readdir(-1)
+	
+	templatePaths := new([]string)
+	for _, pathInfo := range templatePathsRaw {
+		if !pathInfo.IsDir() {
+			*templatePaths = append(*templatePaths, basePath + "/" + pathInfo.Name())
+		}
+	}
+	
+	result.ParseFiles(*templatePaths...)
+	
+	return result
 }
